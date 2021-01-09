ceefff0cd59ab38f82198ca335e8865f1f86f159
Francisco Lopez Tamagno <flopeztamagno@frsf.utn.edu.ar>
2021-01-09 18:33:22 -0300

    Initial commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+    const name = 'Francisco';
+    return (<View>
+        <Text style={styles.headerStyle}>Getting started with react native!</Text>
+        <Text style={styles.subheaderStyle}>My name is {name}</Text>
+    </View>);
+};
+
+const styles = StyleSheet.create({
+    headerStyle: {
+        fontSize: 45
+    },
+    subheaderStyle: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListScreen = () => {
+    return (<View>
+        <Text>List screen</Text>
+    </View>);
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
